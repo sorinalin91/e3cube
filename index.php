35e05d853c5cf839fcdc0a19ac8a0b043ed5032c
Pavel Sorin-Alin <sorinalinpavel@gmail.com>
2015-05-06 02:11:15 +0300

    Started 5-7 grid system on Page 1

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,8 +19,48 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, sdaasd a world!</h1>
-<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+    <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img class="img-responsive" src="images/logo.png"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-5 col-md-5 col-lg-5 block-item no-padding light-background">
+      <div class="dummy" style="padding-top: 80%"></div>
+        <div class="grid-container">
+          <a href="" class="full-div-link"></a>
+        </div>
+    </div>
+    <div class="col-xs-7 col-md-7 col-lg-7 no-padding block-item">
+      <div class="dummy" style="padding-top:57.1%"></div>
+        <div class="grid-container">
+          <a href="" class="full-div-link"></a>
+          <h2 class="image-title">ACCRINGTON &amp; ROSSENDALE COLLEGE NEW SPORTS CENTRE</h2>
+          <img class="full-width-image"src="images/Image_A.jpg">
+        </div>
+    </div>
+  </div>
+</div>  
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
