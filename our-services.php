7d99213cfe0578dd7f685b77e32350ab850b3d2c
Pavel Sorin-Alin <sorinalinpavel@gmail.com>
2015-05-20 01:46:06 +0300

    fixed footer for mobile/tablet

--- our-services.php
+++ our-services.php
@@ -28,7 +28,7 @@
 </div>	
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy" style="padding-top:69%"></div>
+  <div class="dummy our-services-dummy-boxes" style="padding-top:69%"></div>
   <div class="grid-container">
   	<a href="case-study-a.php" class="full-div-link"></a>
     <h2 class="image-title smaler-image-title-mobile">STRATEGIC PLANNING<br />CAPITAL FINDING SUPPORT<br />MASTER PLANNING<br /></h2>
@@ -37,7 +37,7 @@
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy" style="padding-top:69%"></div>
+  <div class="dummy our-services-dummy-boxes" style="padding-top:69%"></div>
   <div class="grid-container">
   	<a href="case-study-a.php" class="full-div-link"></a>
     <h2 class="image-title smaler-image-title-mobile">BUILDING SURVEYING CONSULTANCY</h2>
@@ -46,7 +46,7 @@
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy" style="padding-top:69%"></div>
+  <div class="dummy our-services-dummy-boxes" style="padding-top:69%"></div>
   <div class="grid-container">
   	<a href="case-study-a.php" class="full-div-link"></a>
     <h2 class="image-title smaler-image-title-mobile">DESIGN AND CONTRACT MANAGEMENT</h2>
@@ -55,7 +55,7 @@
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy" style="padding-top:69%"></div>
+  <div class="dummy our-services-dummy-boxes" style="padding-top:69%"></div>
   <div class="grid-container">
   	<a href="case-study-a.php" class="full-div-link"></a>
     <h2 class="image-title smaler-image-title-mobile">ACCESS &amp; EQUALITY CONSULTANCY</h2>
@@ -64,7 +64,7 @@
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy" style="padding-top:69%"></div>
+  <div class="dummy our-services-dummy-boxes" style="padding-top:69%"></div>
   <div class="grid-container">
   	<a href="case-study-a.php" class="full-div-link"></a>
     <h2 class="image-title smaler-image-title-mobile">HEALTH &amp; SAFETY</h2>
@@ -73,7 +73,7 @@
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy" style="padding-top:69%"></div>
+  <div class="dummy our-services-dummy-boxes" style="padding-top:69%"></div>
   <div class="grid-container">
   	<a href="case-study-a.php" class="full-div-link"></a>
     <h2 class="image-title smaler-image-title-mobile">EDUCATION SECTOR</h2>
--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
 <footer>
 <div class="col-xs-4 col-md-4 col-lg-4 footer">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy dummy-footer" style="padding-top:40%"></div>
+  <div class="dummy dummy-footer" style="padding-top:30%"></div>
   <div class="grid-container">
     <a href="case-study-a.php" class="full-div-link"></a>
     <a class="navbar-brand" href="index.php"><img class="img-responsive logo" src="images/logo.png"></a>
@@ -10,22 +10,22 @@
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 footer">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy dummy-footer" style="padding-top:40%"></div>
-  <div class="grid-container">
+  <div class="dummy dummy-footer" style="padding-top:30%"></div>
+  <div class="grid-container footer-grid-container">
     <div class="text-padding filter-hover-text">
-      <p class="padding-20-0-0-50">Contact US</p>
-      <a href="mailto:enquiries@e3cube.co.uk" class="padding-left-50 footer-email"><span>Email:</span> enquiries@e3cube.co.uk</a>
-      <p class="padding-left-50"><span>Tel: </span>03333 058 2918</p>
+      <h2 class="footer-h2">CONTACT US <span style="color:#58b7df;">&nbsp;></span></h2>
+      <h2 class="footer-h2"><a href="mailto:enquiries@e3cube.co.uk" class="footer-email"><span class="hidden-mobile-p">Email:</span> enquiries@e3cube.co.uk</a></h2>
+      <h2 class="footer-h2 footer-tel"><span class="hidden-mobile-p">Tel: </span>03333 058 2918</h2>
     </div>
   </div>
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 footer">
   <div class="nav-bg-filter filter-hover"></div>
-  <div class="dummy dummy-footer" style="padding-top:40%"></div>
-  <div class="grid-container">
+  <div class="dummy dummy-footer" style="padding-top:30%"></div>
+  <div class="grid-container footer-parteners">
     <a href="case-study-a.php" class="full-div-link"></a>
     <a class="navbar-brand" href="#"><img class="img-responsive logo-footer fst" src="images/rics-logo.png"></a>
-    <a class="navbar-brand" href="#"><img class="img-responsive logo-footer" src="images/nrac-logo.png"></a>
+    <!-- <a class="navbar-brand" href="#"><img class="img-responsive logo-footer" src="images/nrac-logo.png"></a> -->
     <a class="navbar-brand" href="#"><img class="img-responsive logo-footer" src="images/aps-logo.png"></a>
     <a class="navbar-brand" href="#"><img class="img-responsive logo-footer" src="images/nebosh-logo.png"></a>
     <a class="navbar-brand" href="#"><img class="img-responsive logo-footer" src="images/iosh-logo.png"></a>
