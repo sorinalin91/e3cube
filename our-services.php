1ef6cd439f94e6110c88596c528412c067f6508f
Pavel Sorin-Alin <sorinalinpavel@gmail.com>
2015-05-31 18:54:56 +0300

    nr. 29

--- our-services.php
+++ our-services.php
@@ -6,21 +6,30 @@
   <div class="dummy our-services-dummy" style="padding-top:69%"></div>
   <div class="grid-container first-box-h2">
 	  <div class="text-padding filter-hover-text top-box our-services-head-section">
-	    <h2 class="padding-20-0-0-50">OUR SERVICES <span style="color:#58b7df;">&nbsp;></span></h2>
+	    <h2 class="padding-20-0-0-50 our-services-alignement">OUR SERVICES<span style="color:#58b7df;">&nbsp;></span></h2>
 	    <span class="blue-border hidden-mobile-p"></span>
 	    <p class="padding-50-0-0-50 our-services-first-p">We take time to understand your needs and integrate them into a bespoke service.</p><br />
-	    <p class="padding-left-50">As a practice we have the full range of disciplines within our team, ensuring you’re in safe hands from concept to completion.</p><br />
 	  </div>
   </div>
 </div>
-<div class="col-xs-6 col-md-6 col-lg-6 no-padding block-item ">
+<div class="col-xs-6 col-md-6 col-lg-6 no-padding block-item">
   <div class="nav-bg-filter filter-hover"></div>
   <div class="dummy our-services-dummy" style="padding-top:69%"></div>
   <div class="grid-container first-box-h2">
 	  <div class="text-padding filter-hover-text top-box">
-      <h2 class="padding-20-0-0-50">&nbsp;</h2>
-	    <span class="blue-border hidden-mobile-p"></span>
-	    <p class="padding-50-0-0-50 our-services-first-p">As a practice we have the full range of skills within our team, ensuring you’re in safe hands from concept to completion.</p><br />
+       <p class="padding-50-0-0-50 first-p-without-title strategic-12-grid-p ul-title-padding ul-title-padding-our-services" style="margin-bottom: 5px; ">OUR SERVICES INCLUDE:</p>
+          <p class="padding-left-50" style="font-style:italic;">
+            <ul class="our-servies-ul-list" >
+              <li>Strategic Planning </li>
+              <li>Capital Funding support</li>
+              <li>Master Planning</li>
+              <li>Building Surveying Consultancy </li>
+              <li>Design &amp; Contract Management</li>
+              <li>Access &amp; Equality Consultancy </li>
+              <li>Health &amp; Safety</li>
+              <li>Education Sector Services</li>
+            </ul>  
+          </p>
 	  </div>
   </div>
 </div>	
@@ -29,7 +38,7 @@
   <div class="dummy our-services-dummy-boxes" style="padding-top:80%"></div>
   <div class="grid-container">
   	<a href="strategic-planning.php" class="full-div-link"></a>
-    <h2 class="image-title smaler-image-title-mobile" style="color:#131e29;font-weight:bold;">STRATEGIC PLANNING<br />CAPITAL FINDING SUPPORT<br />MASTER PLANNING<br /></h2>
+    <h2 class="image-title smaler-image-title-mobile" style="color:#131e29;font-weight:bold;">STRATEGIC PLANNING<br />CAPITAL FUNDING SUPPORT<br />MASTER PLANNING<br /></h2>
     <img class="full-width-image"src="images/ImageD1.jpg">
   </div>
 </div>
@@ -47,8 +56,8 @@
   <div class="dummy our-services-dummy-boxes" style="padding-top:80%"></div>
   <div class="grid-container">
   	<a href="design-and-contract-management.php" class="full-div-link"></a>
-    <h2 class="image-title smaler-image-title-mobile" style="color:#131e29;font-weight:bold;">DESIGN AND CONTRACT MANAGEMENT</h2>
-     <img class="full-width-image"src="images/ImageD3.jpg">
+    <h2 class="image-title smaler-image-title-mobile" style="color:#131e29;font-weight:bold;">DESIGN &amp; CONTRACT MANAGEMENT</h2>
+     <img class="full-width-image  flip-image"src="images/ImageD3.jpg">
   </div>
 </div>
 <div class="col-xs-4 col-md-4 col-lg-4 no-padding block-item">
