3276d5c540d016d410ecad9c61cf3f3c4f95c1e2
Pavel Sorin-Alin <sorinalinpavel@gmail.com>
2015-05-31 20:05:53 +0300

    nr. 45

--- building-surveying.php
+++ building-surveying.php
@@ -6,8 +6,10 @@
   <div class="nav-bg-filter filter-hover"></div>
   <div class="dummy strategic-dummy-mobile" style="padding-top:40%"></div>
     <div class="grid-container strategic-12-col-grid-container">
-      <h2 class="image-title smaler-image-title-mobile ">BUILDING SURVEYING </h2>
-      <h2 class="image-title-12-col-right no-padding smaler-image-title-mobile strategic-12-grid-h2 right-column-mobile-width" style="color:#ffffff;width:38%;">Our experienced Chartered Building Surveyors specialise in the maintenance and care of buildings, offering solutions to mmaintain the value, functionality and statutory compliance of buildings, as an important part of assert management and business continuity for any property owner, Landlord or occupier. </h2>
+      <h2 class="image-title smaler-image-title-mobile ">BUILDING SURVEYING<span style="color:#58b7df;">&nbsp;></span> </h2>
+      <h2 class="image-title-12-col-right no-padding smaler-image-title-mobile strategic-12-grid-h2 right-column-mobile-width strategic-right-box-text" style="color:#ffffff;width:38%;">Our experienced Chartered Building Surveyors specialise in the maintenance and care of buildings, offering solutions to mmaintain the value, functionality and statutory compliance of buildings, as an important part of assert management and business continuity for any property owner, Landlord or occupier. </h2>
+      <h2>&nbsp;</h2>
+      <span class="blue-border hidden-mobile-p"></span>
     </div>
 </div>
 <div class="col-xs-5 col-md-5 col-lg-5 block-item no-padding light-background">
